6a57c2ec7a52935c2c9b8f9e7a2fd3fbfab164f8
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2024-10-05 14:02:41 +0900

    update: setup

--- setup.bat
+++ setup.bat
@@ -6,69 +6,80 @@ setlocal
 set "reg_key=HKLM\SOFTWARE\7-Zip"
 set "ZIP_PATH="
 
-REM 7-Zip���C���X�g�[������Ă��邩
+REM 7-Zipがインストールされているか
 reg query "%reg_key%" >nul 2>&1
 if %errorlevel%==0 (
-    REM 7-Zip��Path���擾
+    REM 7-ZipのPathを取得
     for /f "tokens=2*" %%A in ('reg query "%reg_key%" /v Path 2^>nul') do (
         set "ZIP_PATH=%%B"
     )
 
-    @REM ���W�X�g���̒l����C���X�g�[������Ă��邩�m�F
+    @REM レジストリの値からインストールされているか確認
     if not defined ZIP_PATH (
-        echo 7-Zip���C���X�g�[������Ă��܂���B�C���X�g�[�����J�n���܂��B
-        goto install
+        echo 7-Zipがインストールされていません。インストールを開始します。
+        @REM 7-Zipインストール
+        winget install --id 7zip.7zip -e
+        if %errorlevel%==0 (
+            echo 7-Zipのインストールが完了しました。
+            echo もう一度実行してください。
+            pause
+            exit /b
+        ) else (
+            echo 7-Zipのインストールに失敗しました。
+            pause
+            exit /b
+        )
     )
 ) else (
-    echo 7-Zip���C���X�g�[������Ă��܂���B�C���X�g�[�����J�n���܂��B
-    goto install
+    echo 7-Zipがインストールされていません。インストールを開始します。
+    @REM 7-Zipインストール
+    winget install --id 7zip.7zip -e
+    if %errorlevel%==0 (
+        echo 7-Zipのインストールが完了しました。
+        echo もう一度実行してください。
+        pause
+        exit /b
+    ) else (
+        echo 7-Zipのインストールに失敗しました。
+        pause
+        exit /b
+    )
 )
 
-:install
-@REM 7-Zip�C���X�g�[��
-winget install --id 7zip.7zip -e
-if %errorlevel%==0 (
-    echo 7-Zip�̃C���X�g�[�����������܂����B
-) else (
-    echo 7-Zip�̃C���X�g�[���Ɏ��s���܂����B
-    pause
-    exit /b
-)
-
-@REM �p�b�P�[�W1
+@REM パッケージ1
 set "FILENAME_1=voicevox_engine-windows-nvidia-0.19.1.7z.001"
 set "DOWNLOAD_URL_1=https://github.com/VOICEVOX/voicevox_engine/releases/download/0.19.1/%FILENAME_1%"
 del "%FILENAME_1%"
 curl -L -o "%FILENAME_1%" "%DOWNLOAD_URL_1%"
 
-@REM �p�b�P�[�W2
+@REM パッケージ2
 set "FILENAME_2=voicevox_engine-windows-nvidia-0.19.1.7z.002"
 set "DOWNLOAD_URL_2=https://github.com/VOICEVOX/voicevox_engine/releases/download/0.19.1/%FILENAME_2%"
 del "%FILENAME_2%"
 curl -L -o "%FILENAME_2%" "%DOWNLOAD_URL_2%"
 
-@REM �t�@�C���̉�
+@REM ファイルの解凍
 "%ZIP_PATH%\7z.exe" x "%FILENAME_1%" -o"Voicevox\"
 
-@REM �t�@�C���̈ړ�
+@REM ファイルの移動
 ren "Voicevox\windows-nvidia" "server"
 move /Y "Voicevox" "C:/"
 
-@REM �p�b�P�[�W�t�@�C���̍폜
+@REM パッケージファイルの削除
 del "%FILENAME_1%" "%FILENAME_2%"
 
-@REM �N���p��BAT�t�@�C�����_�E�����[�h
+@REM 起動用のBATファイルをダウンロード
 set "FILENAME_SERVER=voicevox_server.bat"
 set "DOWNLOAD_URL_SERVER=https://github.com/suzuuuuu09/unity-auto-build-voicevox-server/releases/download/0.2.0/%FILENAME_SERVER%"
 
-@REM �N���p��BAT�t�@�C�������݂��Ȃ��ꍇ�_�E�����[�h
+@REM 起動用のBATファイルが存在しない場合ダウンロード
 if not exist %FILENAME_SERVER% (
     curl -L -o "%FILENAME_SERVER%" "%DOWNLOAD_URL_SERVER%"
 )
 
-@REM �t�@�C���̈ړ�
-move /Y "%FILENAME_SERVER%" "C:/Voicevox/"
-echo �Z�b�g�A�b�v���������܂����B
+@REM 起動用のBATファイルのコピー
+copy /Y "%FILENAME_SERVER%" "C:/Voicevox/"
+echo セットアップが完了しました。
 
 endlocal
 pause
